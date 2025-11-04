daf67e8611f3b8169b25b81e0e44d46f7cef744b
Pranav P <153451679+ptowncs@users.noreply.github.com>
2025-11-04 17:28:09 +0000

    endPunctuation method

--- src/Book.java
+++ src/Book.java
@@ -8,29 +8,44 @@ public class Book {
   public String pigLatin(String word) {
     String vowelString = "aeiouy";
     String numberString = " 123456789";
-    
-    if (word.length() == 0 || numberString.contains(word.substring(0, 1))){
+
+    if (word.length() == 0 || numberString.contains(word.substring(0, 1))) {
       return word;
     }
-    if (vowelString.contains((word.substring(0,1)).toLowerCase())){
+    if (vowelString.contains((word.substring(0, 1)).toLowerCase())) {
       return word + "yay";
     } else {
       int index = 0;
       for (int i = 0; i < word.length(); i++) {
-        if (vowelString.contains((word.substring(i,i+1)).toLowerCase())) {
+        if (vowelString.contains((word.substring(i, i + 1)).toLowerCase())) {
           index = i;
           break;
         }
       }
-      return word.substring(index) + word.substring(0,index) + "ay";
+      return word.substring(index) + word.substring(0, index) + "ay";
     }
   }
 
   public int endPunctuation(String word) // return the index of where the punctuation is at the end of a String. If it
                                          // is all punctuation return 0, if there is no punctuation return -1
-  {
+  { 
+    boolean alpha = false;
+    for(int i = 0; i< word.length(); i++){
+      if(Character.isAlphabetic(word.charAt(i))){
+          alpha = true;
+      }else if (!Character.isAlphabetic(word.charAt(i))){
+        if(alpha){
+        alpha = false;
+        return i;
+        }
+      }
+    }
+    if(alpha){
+      return -1;
+    }else{
+      return 0;
+    }
 
-    return -1;
   }
 
   public String translateWord(String word) // to share with class
